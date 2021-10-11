ce93434d5ec953a3811e51d4bfec6b559d7a121c
Vecrou_n <vnechaev00@gmail.com>
2021-10-11 23:10:48 +0300

    some update
    init.sql:
    * добавил таблицу со списком статусов фидбэка
    * переименовал часть полей
    * отформатировал скрипт
    
    docker-compose.yml:
    * добавил конкретную версию образа dpage/pgadmin4, чтобы не перезагружать каждый раз новую версию
    
    ER.png:
    * обновил схему бд

--- db/database/initSQL/init.sql
+++ db/database/initSQL/init.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE SelfRecordingDatabase;
 
 /*
-Длина электронной почты в соответствии с RFC 3696: 
-это ограничение составляет не более 64 символов (октетов) в «локальной части» (до «@») 
-и не более 255 символов (октетов) в доменной части (после «@») 
-общей длиной 320 символов. 
+	Длина электронной почты в соответствии с RFC 3696: 
+	это ограничение составляет не более 64 символов (октетов) в «локальной части» (до «@») 
+	и не более 255 символов (октетов) в доменной части (после «@») 
+	общей длиной 320 символов. 
 */
 CREATE TABLE IF NOT EXISTS USERS(
 	id INTEGER NOT NULL,
@@ -15,24 +15,35 @@ CREATE TABLE IF NOT EXISTS USERS(
 	PRIMARY KEY (id)	
 );
 CREATE TABLE IF NOT EXISTS MESSAGETYPE(
-	messagetype VARCHAR(50) NOT NULL,	
-	PRIMARY KEY (messagetype)	
+	messageType VARCHAR(50) NOT NULL,	
+	PRIMARY KEY (messageType)	
 );
 CREATE TABLE IF NOT EXISTS FEEDBACK(
 	id INTEGER NOT NULL,
 	email VARCHAR(320) NOT NULL,
-	user_ INTEGER REFERENCES USERS(id),
-	type_ VARCHAR(50) NOT NULL REFERENCES MESSAGETYPE(messagetype),
+	userId INTEGER REFERENCES USERS(id),
+	messageType VARCHAR(50) NOT NULL REFERENCES MESSAGETYPE(messageType),
 	text_ TEXT NOT NULL,	
 	PRIMARY KEY (id)	
 );
 CREATE TABLE IF NOT EXISTS HISTORY(
 	id INTEGER NOT NULL,
-	user_ INTEGER NOT NULL REFERENCES USERS(id),
-	login_ TIMESTAMP NOT NULL,
-	logout TIMESTAMP NOT NULL,
+	userId INTEGER NOT NULL REFERENCES USERS(id),
+	login_time TIMESTAMP NOT NULL,
+	logout_time TIMESTAMP NOT NULL,
 	scenes INTEGER NOT NULL,
 	attempts INTEGER NOT NULL,
-	duration INTERVAL NOT NULL,	
+	duration INTERVAL NOT NULL,
+	statusNameINTEGER NOT NULL REFERENCES FEEDBACK_STATUS(id),
+	PRIMARY KEY (id)	
+);
+
+/*
+	желательно записать статусы последовательно, 
+	чтобы продвигать статус обратной связи, инкремируя id названия статуса
+*/
+CREATE TABLE IF NOT EXISTS FEEDBACK_STATUS(
+	id INTEGER NOT NULL,
+	statusName VARCHAR(50) NOT NULL,	
 	PRIMARY KEY (id)	
 );
